83f67f16da4d82b554d54f0382c0769271bbc3aa
Daniel <dringler@mail.uni-mannheim.de>
2016-01-28 15:49:10 +0100

    Lab 1 / Step 3: Input validator

--- client.js
+++ client.js
@@ -15,3 +15,25 @@ window.onload = function(){
 	displayView(welcome);
 
 };
+
+function checkPassword() {
+	var first = document.getElementById('signUpPassword').value;
+	var second = document.getElementById('confirmSignUpPassword').value;
+	var minPwLength = 5;
+	
+	if(first == second &&
+		first.length >= minPwLength ) {
+		document.getElementById('signUpButtonID').disabled = false;
+		//window.alert('Form has been submitted.');
+	} else if (first != second) {
+		document.getElementById('signUpButtonID').disabled = true;
+		// window.alert('Passwords do not match.');
+	} else if (first.length < minPwLength || second.length < minPwLength) {
+		document.getElementById('signUpButtonID').disabled = true;
+		// window.alert('Password must be at least ' + minPwLength + " characters long.");
+	} else {
+		document.getElementById('signUpButtonID').disabled = true;
+		// window.alert('Something went wrong. Please try again.');
+	}
+};
+
